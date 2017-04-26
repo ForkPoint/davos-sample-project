715c85c005e3b9cac5f007b03718d0d5dab9c91b
Ivan Popjelev <ivan@forkpoint.com>
2017-04-26 18:19:18 +0300

    add sample project files

---
+++ app_storefront/cartridge/js/app.js
@@ -0,0 +1,61 @@
+/**
+ *    (c) 2009-2014 Demandware Inc.
+ *    Subject to standard usage terms and conditions
+ *    For all details and documentation:
+ *    https://bitbucket.com/demandware/sitegenesis
+ */
+
+'use strict';
+
+// if jQuery has not been loaded, load from google cdn
+if (!window.jQuery) {
+    var s = document.createElement('script');
+    s.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
+    s.setAttribute('type', 'text/javascript');
+    document.getElementsByTagName('head')[0].appendChild(s);
+}
+
+require('./jquery-ext')();
+
+function initializeEvents() {
+    var controlKeys = ['8', '13', '46', '45', '36', '35', '38', '37', '40', '39'];
+}
+
+/**
+ * @private
+ * @function
+ * @description Adds class ('js') to html for css targeting and loads js specific styles.
+ */
+function initializeDom() {
+    // add class to html for css targeting
+    $('html').addClass('js');
+    if (SitePreferences.LISTING_INFINITE_SCROLL) {
+        $('html').addClass('infinite-scroll');
+    }
+    // load js specific styles
+    util.limitCharacters();
+}
+
+var app = {
+    init: function () {
+        
+    }
+};
+
+// general extension functions
+(function () {
+    String.format = function () {
+        var s = arguments[0];
+        var i, len = arguments.length - 1;
+        for (i = 0; i < len; i++) {
+            var reg = new RegExp('\\{' + i + '\\}', 'gm');
+            s = s.replace(reg, arguments[i + 1]);
+        }
+        return s;
+    };
+})();
+
+// initialize app
+$(document).ready(function () {
+    app.init();
+});
